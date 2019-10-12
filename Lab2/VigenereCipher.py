f0464ce4233aa6df57cb784d7bb775282653370d
mondo192 <31599023+mondo192@users.noreply.github.com>
2019-10-12 11:12:31 +0100

    :tada:

--- Lab2/VigenereCipher.py
+++ Lab2/VigenereCipher.py
@@ -2,35 +2,23 @@ import os
 
 
 class VigenereCipher:
-    def __init__(self):
-        while True:
-            try:
-                self.file_path = 'data/plaintext/message.txt'
-                # self.file_path = self.get_file()
-                with open(self.file_path) as file:
-                    self.file = file.read()
-                    filename = os.path.basename(self.file_path)
-                    print(f'{filename} was found!')
-                    break
-            except OSError as e:
-                print(e)
-        while True:
-            try:
-                self.alphabet_path = 'data/alphabet/english.txt'
-                with open(self.alphabet_path) as alphabet:
-                    self.alphabet = alphabet.read()
-                    filename = os.path.basename(self.alphabet_path)
-                    print(f'{filename} was found!')
-                    break
-            except OSError as e:
-                print(e)
+    def __init__(self, m, a):
+        try:
+            self.message = self.get_data(file_path=m)
+            self.alphabet = self.get_data(file_path=a)
+        except OSError as e:
+            print(e)
 
-        self.ciphertext = self.encrypt('4', self.file)
-        # self.plaintext = self.decrypt(4, self.file)
-        print(self.ciphertext)
-        # print(self.plaintext)
+    @staticmethod
+    def get_data(file_path):
+        with open(file_path) as file:
+            data = file.read()
+            filename = os.path.basename(file_path)
+            print(f'{filename} was found!')
+        return data
 
-    def encrypt(self, key, plaintext):
+    def encrypt(self, key):
+        plaintext = self.message
         key_length = len(key)
         key_as_int = [ord(i) for i in key]
         plaintext_int = [ord(i) for i in plaintext]
@@ -40,7 +28,8 @@ class VigenereCipher:
             ciphertext += chr(value + 65)
         return ciphertext
 
-    def decrypt(self, key, ciphertext):
+    def decrypt(self, key):
+        ciphertext = self.message
         key_length = len(key)
         key_as_int = [ord(i) for i in key]
         ciphertext_int = [ord(i) for i in ciphertext]
@@ -52,4 +41,24 @@ class VigenereCipher:
 
 
 if __name__ == '__main__':
-    app = VigenereCipher()
+    while True:
+        message = input('Enter path to file or (q) to quit: ')
+        alphabet = input('Enter path to alphabet or (q) to quit: ')
+        if message in 'qQ' or alphabet in 'qQ':
+            break
+        vigenere = VigenereCipher(m=message, a=alphabet)
+        if vigenere:
+            while True:
+                menu = input('Vigenere Cipher\n'
+                             '(E)ncrypt message\n'
+                             '(D)ecrypt message\n'
+                             '(Q)uit\n\n'
+                             'Enter your choice (E/D/Q): ')
+                if menu in 'eE':
+                    key = int(input('Enter your encryption key: '))
+                    vigenere.encrypt(k=key)
+                elif menu in 'dD':
+                    key = int(input('Enter the decryption key: '))
+                    vigenere.decrypt(k=key)
+                elif menu in 'qQ':
+                    break
