f07eabcae60bf56d1e1060419b5a22de02f2d9cf
Eamonn Keogh <ekeogh@debian.home>
2019-10-12 21:24:52 +0100

    :tada:

--- Lab2/caesar.py
+++ Lab2/caesar.py
@@ -3,6 +3,13 @@
 import sys
 
 
+def write_data_to_disk(data):
+    workfile = input('\nSave file as: ')
+    with open(workfile, 'w') as f:
+        f.write(data)
+        f.close()
+
+
 def validate_key():
     # confirm exactly two arguments entered on command line (program name and keyword)
     if len(sys.argv) != 2:
@@ -52,10 +59,30 @@ if __name__ == "__main__":
     if not key:
         sys.exit(0)
 
-    plaintext = input("Enter text to be encrypted: ")
-    encrypted = encrypt(plaintext, key)
-    print('Encrypting "' + plaintext + '" with a key of "' + str(key) + '," using Caesar’s cipher, yields the ciphertext "' + encrypted + '."')
-
-    ciphertext = input("Enter text to be decrypted: ")
-    decrypted = decrypt(ciphertext, key)
-    print('Decrypting "' + ciphertext + '" with a key of "' + str(key) + '," using Caesar’s cipher, yields the plaintext "' + decrypted + '."')
+    running = True
+    while running:
+        menu = int(input(
+            '\n1. Encrypt file\n'
+            '2. Decrypt file\n'
+            '3. Exit\n\n'
+            'Enter your choice: '
+        ))
+        if menu == 1:
+            workfile = input('Enter the path to the file for encryption: ')
+            with open(workfile) as f:
+                plaintext = f.read()
+                encrypted = encrypt(plaintext, key)
+                print('\nEncrypting...\n"' + plaintext + '"\n\nKey: ' + str(key) + '\n\nCiphertext...\n' + encrypted)
+                write_data_to_disk(encrypted)
+        elif menu == 2:
+            file = input('\nEnter the path to the file for decryption: ')
+            with open(file) as f:
+                ciphertext = f.read()
+                decrypted = decrypt(ciphertext, key)
+                print('\nDecrypting...\n"' + ciphertext + '"\n\nKey: ' + str(key) + '\n\nPlaintext...\n' + decrypted)
+                write_data_to_disk(decrypted)
+        elif menu == 3:
+            print('Exiting...')
+            running = False
+        else:
+            print('Invalid selection. Try again')
