5299e2bf048ecffc0d88f7d003cb1d7177774ae9
Eamonn Keogh <ekeogh@debian.home>
2019-11-05 22:29:01 +0000

    :tada

--- Lab3/miller_rabin.py
+++ Lab3/miller_rabin.py
@@ -1,11 +1,10 @@
+#!/usr/bin/python3
 from random import randint
 
 
 def miller_rabin_test(n):
     q = n - 1
     k = 1
-
-    print(f'Testing: {n}')
 
     # find integers k, q with k > 0, q being odd so that (n - 1) = 2 ^ k * q
     while q % 2 == 0:
@@ -17,23 +16,17 @@ def miller_rabin_test(n):
 
     # if a ^ q mod n = 1 then return inconclusive
     if pow(a, q, n) == 1:
-        return "Inconclusive"
+        return 'Inconclusive, probably not a prime number.'
 
     # if a ^ (2 ^ i) * q mod n = n - 1 then return inconclusive
-    for i in range(k - 1):
+    for i in range(1, k):
         if pow(a, (pow(2, i) * q), n) == n - 1:
-            return "Inconclusive"
+            return 'Inconclusive, probably not a prime number.'
 
     # return conclusive
-    return "Composite"
+    return 'Composite, definitely not a prime number.'
 
 
 if __name__ == '__main__':
-    print(miller_rabin_test(101))
-    print(miller_rabin_test(592701729979))
-    # print(f'Result: {miller_rabin_test(101)}')
-    # print(f'Result: {miller_rabin_test(592701729979)}')
-
-
-
-
+    print(f'The result for 101: {miller_rabin_test(101)}')
+    print(f'The result for 592701729979: {miller_rabin_test(592701729979)}')
